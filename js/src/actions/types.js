314751950cb2a5137b7c7e7c69425c4e35e002de
John <johnschings@aim.com>
2020-04-27 17:37:42 -0400

    Testing

--- js/src/actions/types.js
+++
@@ -1,9 +0,0 @@
-export const EMAIL_CHANGED = 'email_changed';
-export const PASSWORD_CHANGED = 'password_changed';
-export const LOGIN_USER_SUCCESS = 'login_user_success';
-export const LOGIN_USER_FAIL = 'login_user_fail';
-export const LOGIN_USER = 'login_user';
-
-export const POST_EDIT = 'post_edit';
-export const POST_CREATE = 'post_create';
-export const POSTS_FETCH_SUCCESS = 'posts_fetch_success';
